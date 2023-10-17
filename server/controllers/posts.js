948dabb8ae1bbbc8ad716c330c0ec76c3454e019
Soros87 <timkwan87@gmail.com>
2023-10-17 10:24:42 +0800

    set up controller component in server

---
+++ server/controllers/posts.js
@@ -0,0 +1,5 @@
+//controller is to apply the logic of the req,res
+
+export const getPosts = async (req, res) => {
+  res.send('THIS WORKS')
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,10 @@
 import express from 'express'
+import {getPosts} from '../controllers/posts.js'
 
 const router = express.Router();
 
 //call back function that is executed once someone visits the route in localhost:5100
-router.get('/',(req,res)=>{
-res.send('THIS WORKS')
-})
+router.get('/',getPosts)
+
 
 export default router;
