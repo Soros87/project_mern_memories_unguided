c8c1abf4c0e73cd1ec83e3276d096e036efc13a4
Soros87 <timkwan87@gmail.com>
2023-10-17 09:53:40 +0800

    setup server index.js and connect to db

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom'
+import {createRoot} from 'react-dom/client'
+import {Provider} from 'react-redux'
+import {createStore,applyMiddleware,compose} from 'redux'
+import thunk from 'redux-thunk'
+
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
