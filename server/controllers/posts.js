8a1f4e059b934478c5d489b36f27a61fa6d515ed
Soros87 <timkwan87@gmail.com>
2023-10-17 13:26:39 +0800

    create getPosts logic in server

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,17 @@
+import PostMessage from '../models/postMessage.js'
+
 //controller is to apply the logic of the req,res
 
 export const getPosts = async (req, res) => {
-  res.send('THIS WORKS')
+  try {
+    const postMessages = await PostMessage.find()
+    
+    res.status(200).json(postMessages)
+  } catch(error) {
+    res.status(404).json({message:error.message})
+  }
+}
+
+export const createPost = async (req,res) => {
+  res.send('Post Creation')
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import {getPosts} from '../controllers/posts.js' //remember to add .js - in react we dont need. in express we need
+import {getPosts,createPost} from '../controllers/posts.js' //remember to add .js - in react we dont need. in express we need
 
 const router = express.Router();
 
 //call back function that is executed once someone visits the route in localhost:5100
 router.get('/',getPosts)
-
+router.get('/',createPost)
 
 export default router;
