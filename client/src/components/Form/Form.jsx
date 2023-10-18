1908de71e5dc7ea485b599675fabaa64528ac665
Soros87 <timkwan87@gmail.com>
2023-10-18 09:43:30 +0800

    create the api to connect client to server

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -10,11 +10,10 @@ const Form = () => {
       <div className="p-5 sm:w-96 w-full flex-col justify-start items-center ">
         <form autoComplete="off" onSubmit={handleSubmit} >
           <div className="text-white pb-2 font-mono text-2xl">Post it!</div>
-          <input placeholder="Creator" name="Creator" type="text" className="my-2 w-full p-3 outline-none bg-gray-50 text-white border-none text-sm white-glassmorphism rounded-lg font-mono" />
+          <input placeholder="creator" name="creator" type="text" className="my-2 w-full p-3 outline-none bg-gray-50 text-white border-none text-sm white-glassmorphism rounded-lg font-mono" />
           <input placeholder="title" name="title" type="text" className="my-2 w-full p-3 outline-none bg-gray-50 text-white border-none text-sm white-glassmorphism rounded-lg font-mono" />
           <input placeholder="message" name="message" type="text" className="my-2 w-full p-3 outline-none bg-gray-50 text-white border-none text-sm white-glassmorphism rounded-lg font-mono" />
-          <input placeholder="tags" name="tags" type="text" className="my-2 w-full p-3 outline-none bg-gray-50 text-white border-none text-sm white-glassmorphism rounded-lg font-mono" />
-          
+          <input placeholder="tags" name="tags" type="text" className="my-2 w-full p-3 outline-none bg-gray-50 text-white border-none text-sm white-glassmorphism rounded-lg font-mono" />  
           <div className="w-[97%] my-5"><FileBase type="file" multiple={false} onDone={() => (any)} /></div>
           <button type="submit" className="m-2 text-white bg-[#2952e3] w-full  rounded-full cursor-pointer px-4 py-2 min-h-[40px] font-mono">Submit</button>
           <button type="submit" className="m-2 text-white bg-[#2952e3] w-full  rounded-full cursor-pointer  px-4 py-2 min-h-[40px] font-mono" onClick={clear}>Clear</button>
