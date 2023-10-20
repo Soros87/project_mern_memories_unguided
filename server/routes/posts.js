51f2e863f96b2599d01206a1d49f1e7edad1eb8f
Soros87 <timkwan87@gmail.com>
2023-10-20 17:21:18 +0800

    implement deletePost in server

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {getPosts,createPost,updatePost} from '../controllers/posts.js' //remember to add .js - in react we dont need. in express we need
+import {getPosts,createPost,updatePost,deletePost} from '../controllers/posts.js' //remember to add .js - in react we dont need. in express we need
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/',getPosts)
 router.post('/',createPost)
 router.patch('/:id', updatePost)
+router.delete('/:id', deletePost);
 
 export default router;
