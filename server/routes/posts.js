28d00f31c107c37559850cde3aa93796d7fd5663
Soros87 <timkwan87@gmail.com>
2023-10-20 15:03:57 +0800

    implement server updatePost

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import {getPosts,createPost} from '../controllers/posts.js' //remember to add .js - in react we dont need. in express we need
+import {getPosts,createPost,updatePost} from '../controllers/posts.js' //remember to add .js - in react we dont need. in express we need
 
 const router = express.Router();
 
 //call back function that is executed once someone visits the route in localhost:5100
 router.get('/',getPosts)
 router.post('/',createPost)
+router.patch('/:id', updatePost)
 
 export default router;
